b2fe258df88fa0b8e811b1e92ddc3eafa578762f
Achxter <hansphilemon@gmail.com>
2024-09-17 21:20:43 +0700

    akhir, semoga bener

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_04
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -17,11 +18,16 @@ val TABS_CONTENT_FIXED = listOf(
     R.string.kopikenangan_desc,
 )
 
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+class CafeAdapter(
+    private val context: Context,
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
+
     override fun createFragment(position: Int): Fragment {
-        return CafeDetailFragment()
+        return CafeDetailFragment.newInstance(context.getString(TABS_CONTENT_FIXED[position]))
     }
 }
