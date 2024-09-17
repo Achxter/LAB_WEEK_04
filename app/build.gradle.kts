cf29ea68f76aa7f001ffa792439b49637342d9d0
Achxter <hansphilemon@gmail.com>
2024-09-17 21:30:09 +0700

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.junit)
 }
